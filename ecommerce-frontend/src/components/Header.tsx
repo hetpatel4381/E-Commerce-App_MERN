07456965da042856b810340c2dbe01456b86a94d
hetpatel4381 <hetpatel2808hp@gmail.com>
2024-01-31 00:08:03 +0530

    header created

---
+++ ecommerce-frontend/src/components/Header.tsx
@@ -0,0 +1,54 @@
+import { useState } from "react";
+import { Link } from "react-router-dom";
+import {
+  FaSearch,
+  FaShoppingBag,
+  FaSignInAlt,
+  FaUser,
+  FaSignOutAlt,
+} from "react-icons/fa";
+
+const user = { _id: "Het", role: "admin" };
+
+const Header = () => {
+  const [isOpen, setIsOpen] = useState<boolean>(false);
+
+  return (
+    <nav className="header">
+      <Link to={"/"}>Home</Link>
+      <Link to={"/search"}>
+        <FaSearch />
+      </Link>
+      <Link to={"/cart"}>
+        <FaShoppingBag />
+      </Link>
+
+      {user?._id ? (
+        <>
+          <button onClick={() => setIsOpen((prev) => !prev)}>
+            <FaUser />
+          </button>
+
+          <dialog open={isOpen}>
+            <div>
+              {user.role === "admin" && (
+                <Link to={"/admin/dashboard"}>Admin</Link>
+              )}
+
+              <Link to={"/orders"}>Orders</Link>
+              <button>
+                <FaSignOutAlt />
+              </button>
+            </div>
+          </dialog>
+        </>
+      ) : (
+        <Link to={"/login"}>
+          <FaSignInAlt />
+        </Link>
+      )}
+    </nav>
+  );
+};
+
+export default Header;
