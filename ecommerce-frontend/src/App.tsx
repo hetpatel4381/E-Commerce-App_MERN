a3dca4a8064be79d78db9455c29e658549ded863
hetpatel4381 <hetpatel2808hp@gmail.com>
2024-01-30 23:17:49 +0530

    frontend setup completed with routing for 3 pages

---
+++ ecommerce-frontend/src/App.tsx
@@ -0,0 +1,24 @@
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+
+import { lazy } from "react";
+const Home = lazy(() => import("./pages/Home"));
+const Search = lazy(() => import("./pages/Search"));
+const Cart = lazy(() => import("./pages/Cart"));
+
+// import Home from "./pages/Home";
+// import Search from "./pages/Search";
+// import Cart from "./pages/Cart";
+
+const App = () => {
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/search" element={<Search />} />
+        <Route path="/cart" element={<Cart />} />
+      </Routes>
+    </Router>
+  )
+}
+
+export default App;
