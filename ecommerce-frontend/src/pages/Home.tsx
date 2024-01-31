995e325eb0712a3ecb031be87d48bce8e3f7bd54
hetpatel4381 <hetpatel2808hp@gmail.com>
2024-01-31 12:27:15 +0530

    Home page UI implemented

--- ecommerce-frontend/src/pages/Home.tsx
+++ ecommerce-frontend/src/pages/Home.tsx
@@ -1,7 +1,32 @@
+import { Link } from "react-router-dom";
+import ProductCard from "../components/ProductCard";
+
 const Home = () => {
+  const addToCartHandler = () => {};
+
   return (
-    <div>Home</div>
-  )
-}
+    <div className="home">
+      <section></section>
+
+      <h1>
+        Latest Products
+        <Link to={"/search"} className="findmore">
+          More
+        </Link>
+      </h1>
+
+      <main>
+        <ProductCard
+          productId="hetpatel"
+          price={3000}
+          stock={435}
+          name="Macbook"
+          handler={addToCartHandler}
+          photo="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRzdDt-muZcwGPOftRj3nYz8ZIPfg3Vdepxbw&usqp=CAU"
+        />
+      </main>
+    </div>
+  );
+};
 
 export default Home;
