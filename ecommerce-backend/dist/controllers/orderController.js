e1954f12a3dcfd69d36c3e15823d0c6a53172029
hetpatel4381 <hetpatel2808hp@gmail.com>
2024-02-05 13:51:33 +0530

    created order model and it's types

--- ecommerce-backend/dist/controllers/orderController.js
+++ ecommerce-backend/dist/controllers/orderController.js
@@ -1,2 +1,3 @@
 import { TryCatch } from "../middlewares/error.js";
-export const newOrder = TryCatch(async (req, res, next) => { });
+export const newOrder = TryCatch(async (req, res, next) => {
+});
